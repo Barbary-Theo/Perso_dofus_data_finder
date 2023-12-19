db8b562bcdc7e780265e5cbcecbe30f85898c4d7
Barbary-Theo <theo.barbary25@gmail.com>
2023-12-19 16:59:36 +0100

    rename non-standard name file

--- data_scraping/requester.py
+++ data_scraping/requester.py
@@ -4,8 +4,8 @@ import requests
 import random
 import time
 
-from Database_connector import DatabaseConnector
-from Type import Type
+from database_connector import DatabaseConnector
+from type import Type
 from html_parser import HTMLParser
 from text_parser import TextParser
 
